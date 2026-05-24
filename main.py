b746e5c2b32f178362a65f5ba1012a67a61be940
Judres-s <julianq397@gmail.com>
2026-05-24 14:40:04 -0500

    leer

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+mi_app= FastAPI()
+
+@mi_app.get("/")
+def msg_clientes():
+    return{"Este es el proyecto de mis clientes."}
+
+
+@mi_app.get("/Lista")
+def de_clientes():
+    return ["Y mis clientes son:", "Marco","Antonio","Ximena","Cecilia","Alejandra","Sofia"]
+
+
+
+#https://github.com/Judres-s/Proyecto_clientes.git
