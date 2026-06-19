8b122de362f1c0aee74aeb2377a8efaff8fd6f18
Judres-s <julianq397@gmail.com>
2026-06-19 16:09:24 -0500

    Se añadio biblioteca y capturacion de errores junto a clase cliente

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException #HTTPException es para capturar errors
+from pydantic import BaseModel #Biblioteca para definir modelos de datos
 
 mi_app= FastAPI()
 
-@mi_app.get("/")
-def msg_clientes():
-    return{"Este es el proyecto de mis clientes."}
+@mi_app.get("/")        
+def msg_clientes(): #Función
+    return{"Este es el proyecto de mis clientes."} #Valor retornado
 
 
 @mi_app.get("/Lista")
 def de_clientes():
     return ["Y mis clientes son:", "Marco","Antonio","Ximena","Cecilia","Alejandra","Sofia"]
+
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    descripcion: str
+
