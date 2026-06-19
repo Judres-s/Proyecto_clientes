1350c3e4262c08b2e3ff8e6d680cf0b0affbd764
Judres-s <julianq397@gmail.com>
2026-06-19 16:53:27 -0500

    Lista de clientes

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def msg_clientes(): #Función
     return{"Este es el proyecto de mis clientes."} #Valor retornado
 
 
-@mi_app.get("/Lista")
-def de_clientes():
-    return ["Y mis clientes son:", "Marco","Antonio","Ximena","Cecilia","Alejandra","Sofia"]
+lista_clientes = [
+    {"id": 1, "nombre": "Lady", "email": "lady@gmail.com", "edad": 22, "descripcion": "NA"},
+    {"id": 2, "nombre": "Luis", "email": "luis@gmail.com", "edad": 19, "descripcion": "NA"},
+    {"id": 3, "nombre": "Ana", "email": "ana@gmail.com", "edad": 23, "descripcion": "NA"},
+    {"id": 4, "nombre": "Carlos", "email": "carlos@gmail.com", "edad": 25, "descripcion": "NA"},
+    {"id": 5, "nombre": "Sofía", "email": "sofia@gmail.com", "edad": 21, "descripcion": "NA"},
+]
 
 class Cliente(BaseModel):
     id: int
@@ -18,6 +22,13 @@ class Cliente(BaseModel):
     email: str
     descripcion: str
 
-class Factura(BaseModel): id: int 
-cliente_id: int 
-vrtotal: float
+class Factura(BaseModel): 
+    id: int 
+    cliente_id: int 
+    vrtotal: float
+    
+class Transaccion(BaseModel): 
+    id: int 
+    vr_unitario: float 
+    cantidad: int 
+    factura_id: int
