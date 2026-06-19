564395a6d6d08ca847de44a8f66082cd20fb4173
Judres-s <julianq397@gmail.com>
2026-06-19 16:55:20 -0500

    Listar clientes y estructura de clases cambiada

--- main.py
+++ main.py
@@ -3,19 +3,6 @@ from pydantic import BaseModel #Biblioteca para definir modelos de datos
 
 mi_app= FastAPI()
 
-@mi_app.get("/")        
-def msg_clientes(): #Función
-    return{"Este es el proyecto de mis clientes."} #Valor retornado
-
-
-lista_clientes = [
-    {"id": 1, "nombre": "Lady", "email": "lady@gmail.com", "edad": 22, "descripcion": "NA"},
-    {"id": 2, "nombre": "Luis", "email": "luis@gmail.com", "edad": 19, "descripcion": "NA"},
-    {"id": 3, "nombre": "Ana", "email": "ana@gmail.com", "edad": 23, "descripcion": "NA"},
-    {"id": 4, "nombre": "Carlos", "email": "carlos@gmail.com", "edad": 25, "descripcion": "NA"},
-    {"id": 5, "nombre": "Sofía", "email": "sofia@gmail.com", "edad": 21, "descripcion": "NA"},
-]
-
 class Cliente(BaseModel):
     id: int
     nombre: str
@@ -32,3 +19,19 @@ class Transaccion(BaseModel):
     vr_unitario: float 
     cantidad: int 
     factura_id: int
+
+
+
+lista_clientes = [
+    {"id": 1, "nombre": "Lady", "email": "lady@gmail.com", "edad": 22, "descripcion": "NA"},
+    {"id": 2, "nombre": "Luis", "email": "luis@gmail.com", "edad": 19, "descripcion": "NA"},
+    {"id": 3, "nombre": "Ana", "email": "ana@gmail.com", "edad": 23, "descripcion": "NA"},
+    {"id": 4, "nombre": "Carlos", "email": "carlos@gmail.com", "edad": 25, "descripcion": "NA"},
+    {"id": 5, "nombre": "Sofía", "email": "sofia@gmail.com", "edad": 21, "descripcion": "NA"},
+]
+
+@app.get("/clientes")
+def listar_clientes():
+     return lista_clientes
+     
+
